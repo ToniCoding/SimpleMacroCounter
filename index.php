4d06556312baf257b8cdadcb72772c8396eeb769
ToniCoding <antoniovallejo2612@gmail.com>
2025-07-29 22:21:49 +0200

    temp(v0.2.0): Index test modifications.

--- index.php
+++ index.php
@@ -1,10 +1,31 @@
 <?php
 
-require_once __DIR__ . "/app/controller/MacroCounterController.php";
+// INDEX IS STILL USED TO TEST FUNCTIONALITY.
 
-$macroController = new MacroCounterController();
-$macros = $macroController->createMacros(["protein","carbs", "fat"], [140, 150, 64], [120, 150, 60]);
+//require_once __DIR__ . "/app/controller/MacroCounterController.php";
+//require_once __DIR__ . '/app/controller/UserController.php';
+//require_once __DIR__ . '/app/model/User.php';
 
-$macroController->displayMacrosAndCalories($macros);
-$macroController->displayIngestedMacrosForm();
-$macroController->displayDate();
+//$macroController = new MacroCounterController();
+//$macros = $macroController->createMacros(["protein","carbs", "fat"], [140, 150, 64], [120, 150, 60]);
+
+//$macroController->displayMacrosAndCalories($macros);
+//$macroController->displayIngestedMacrosForm();
+//$macroController->displayDate();
+
+/**
+ * Create a user object.
+ */
+//$dtTestObj = DateTime::createFromFormat("d-m-Y", "28-07-2025");
+//$userController = new UserController();
+//$userModel = new User("Nerea", "1234","Neneta", "ner@neneta.org", 21, $dtTestObj, $dtTestObj, true);
+
+/**
+ * User administration actions
+ *      - Create a new user.
+ *      - Retrieve data of a selected user.
+ *      - Delete a user.
+*/
+//$userController->createUser($userModel);
+//$userController->retrieveUser($userModel);
+//$userController->deleteUser($userModel);
