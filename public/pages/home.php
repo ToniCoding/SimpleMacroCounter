9179f5fc824862b55997c2e4c2399428d5452a19
ToniCoding <antoniovallejo2612@gmail.com>
2025-09-14 12:27:37 +0200

    chore(v0.3.0): Add favicon references

--- public/pages/home.php
+++ public/pages/home.php
@@ -4,13 +4,13 @@
 
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="icon" href="public/imag/favicon.ico" type="image/x-icon">
-    <title>Home - Simple Macro Counter</title>
-</head>
-<body>
-    <h3>Welcome to SMC main page</h3>
-</body>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="icon" href="public/imag/favicon.ico" type="image/x-icon">
+        <title>Home - Simple Macro Counter</title>
+    </head>
+    <body>
+        <h3>Welcome to SMC main page</h3>
+    </body>
 </html>
