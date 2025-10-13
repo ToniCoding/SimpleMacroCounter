c5f71849f9eb0bc6c0cb11d60b1134e512c8699b
ToniCoding <antoniovallejo2612@gmail.com>
2025-10-13 14:01:22 +0200

    feat(v0.5.0): Added registered and logged in user DTOs

---
+++ src/DTO/RegisterUserDTO.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\DTO;
+
+use Symfony\Component\Validator\Constraints as Assert;
+
+class RegisterUserDTO {
+    #[Assert\NotBlank]
+    private string $username;
+
+    #[Assert\NotBlank]
+    private string $password;
+
+    #[Assert\NotBlank]
+    #[Assert\Email]
+    private string $email;
+
+    #[Assert\NotBlank]
+    private string $alias;
+
+    #[Assert\NotBlank]
+    private int $age;
+
+    public function getUsername(): string {
+        return $this->username;
+    }
+
+    public function setUsername(string $newUsername): void {
+        $this->username = $newUsername;
+    }
+
+    public function getPassword(): string {
+        return $this->password;
+    }
+
+    public function setPassword(string $newPassword): void {
+        $this->password = $newPassword;
+    }
+
+    public function getEmail(): string {
+        return $this->email;
+    }
+
+    public function setEmail(string $newEmail): void {
+        $this->email = $newEmail;
+    }
+
+    public function getAlias(): string {
+        return $this->alias;
+    }
+
+    public function setAlias(string $newAlias): void {
+        $this->alias = $newAlias;
+    }
+
+    public function getAge(): int {
+        return $this->age;
+    }
+
+    public function setAge(int $newAge): void {
+        $this->age = $newAge;
+    }
+}
