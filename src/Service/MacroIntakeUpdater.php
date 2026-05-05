d0a8b4b36093e9a4763103b15372a2807c70e3fe
ToniCoding <antoniovallejo2612@gmail.com>
2026-05-05 22:40:07 +0200

    fix(v0.6.0): Reducing more macro-nutrients than ingested was possible

--- src/Service/MacroIntakeUpdater.php
+++ src/Service/MacroIntakeUpdater.php
@@ -10,7 +10,10 @@ use src\Exceptions\ExceededMacroLimitException;
 use src\Repository\KcalsDailyRepository;
 
 class MacroIntakeUpdater extends ServiceEntityRepository {
-    public function __construct(private KcalsDailyRepository $kcalsDailyRepository) {}
+    public function __construct(
+        private KcalsDailyRepository $kcalsDailyRepository,
+        private UserMacrosRetrieve $userMacrosRetrieve
+        ) {}
 
     public function updateMacroIntake(User $user, MacroDataDTO $macroDataDTO, string $intent = 'add'): bool {
         $dataProtein = $macroDataDTO->getProtein();
@@ -19,9 +22,19 @@ class MacroIntakeUpdater extends ServiceEntityRepository {
         $dataFiber = $macroDataDTO->getFiber();
 
         $dataMacros = [$dataProtein, $dataCarbs, $dataFats, $dataFiber];
-
+        $currentMacros = $this->userMacrosRetrieve->getConsumedMacros($user);
+        $dataMacrosConsumedAsArray = [$currentMacros->getProtein(), $currentMacros->getCalories(), $currentMacros->getFats(), $currentMacros->getFiber()];
+        
         if (array_any($dataMacros, fn($v) => $v > 400)) {
             throw new ExceededMacroLimitException("You cannot $intent more than 400 of one macro-nutrient in one intake.");
+        }
+
+        if ($intent == 'reduce') {
+            foreach($dataMacros as $ind => $macro) {
+                if ($macro < $dataMacrosConsumedAsArray[$ind]) {
+                    throw new ExceededMacroLimitException('You cannot reduce more than you have consumed.');
+                }
+            }
         }
 
         $macroDataDTO->setCalories(
