a19b5d607c5dd870cc51b698b0ce53dc0c161d55
ToniCoding <antoniovallejo2612@gmail.com>
2026-06-03 22:34:41 +0200

    refactor(v0.6.1): combine add and reduce pages into one

--- src/Controller/MacroUpdateController.php
+++ src/Controller/MacroUpdateController.php
@@ -7,20 +7,27 @@ use src\Exceptions\ExceededMacroLimitException;
 use src\Form\ModifyMacrosType;
 use src\Service\MacroIntakeUpdater;
 
-use Doctrine\ORM\EntityManagerInterface;
-
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\{Request, Response};
 use Symfony\Component\Routing\Annotation\Route;
 
 class MacroUpdateController extends AbstractController {
     public function __construct(
-        private EntityManagerInterface $entityManager,
         private MacroIntakeUpdater $macroIntakeUpdater,
     ) {}
 
     #[Route(['/modifyMacros', '/modifymacros'], name: 'modifyMacros', methods: ['GET', 'POST'])]
     public function modifyMacros(Request $request): Response {
+        $action = $request->query->get('action');
+
+        return match ($action) {
+            'increase' => $this->addMacros($request),
+            'reduce' => $this->reduceMacros($request),
+            default => $this->redirectToRoute('home')
+        };
+    }
+
+    private function handleMacrosModification(Request $request, string $intent, string $pageTitle): Response {
         $user = $this->getUser();
 
         $form = $this->createForm(ModifyMacrosType::class, new MacroDataDTO(0, 0, 0, 0, 0));
@@ -28,7 +35,10 @@ class MacroUpdateController extends AbstractController {
 
         if ($form->isSubmitted() && $form->isValid()) {
             try {
-                $this->macroIntakeUpdater->updateMacroIntake($user, $form->getData());
+                $method = $intent === 'increase' ? 'addMacros' : 'reduceMacros';
+                $extraParam = $intent === 'reduce' ? ['reduce'] : [];
+                $this->macroIntakeUpdater->$method($user, $form->getData(), ...$extraParam);
+
                 return $this->redirectToRoute('home');
             } catch (ExceededMacroLimitException $ex) {
                 $this->addFlash('modifyMacrosStatus', $ex->getMessage());
@@ -37,32 +47,16 @@ class MacroUpdateController extends AbstractController {
 
         return $this->render('modifyData/ModifyMacrosTemplate.twig', [
             'form' => $form,
-            'page_title' => 'Add macros - SMC',
-            'page_intent' => 'Add macro-nutrient count',
+            'page_title' => $pageTitle,
+            'page_intent' => $intent === 'increase' ? 'Add macro-nutrient count' : 'Reduce macro-nutrient count',
         ]);
     }
 
-    #[Route(['/reduceMacros', '/reducemacros'], name: 'reduceMacros', methods: ['GET', 'POST'])]
-    public function reduceMacros(Request $request): Response {
-        $this->isGranted('IS_AUTHENTICATED_FULLY');
-        $user = $this->getUser();
+    private function addMacros(Request $request): Response {
+        return $this->handleMacrosModification($request, 'increase', 'Add macros - SMC');
+    }
 
-        $form = $this->createForm(ModifyMacrosType::class, new MacroDataDTO(0, 0, 0, 0, 0));
-        $form->handleRequest($request);
-
-        if ($form->isSubmitted() && $form->isValid()) {
-            try {
-                $this->macroIntakeUpdater->updateMacroIntake($user, $form->getData(), 'reduce');
-                return $this->redirectToRoute('home');
-            } catch (ExceededMacroLimitException $ex) {
-                $this->addFlash('modifyMacrosStatus', $ex->getMessage());
-            }
-        }
-
-        return $this->render('modifyData/ModifyMacrosTemplate.twig', [
-            'form' => $form,
-            'page_title' => 'Reduce macros - SMC',
-            'page_intent' => 'Reduce macro-nutrient count'
-        ]);
+    private function reduceMacros(Request $request): Response {
+        return $this->handleMacrosModification($request, 'reduce', 'Reduce macros - SMC');
     }
 }
