fdd4ad2f6015da270b57b02c7c39148944a2b765
ToniCoding <antoniovallejo2612@gmail.com>
2026-06-27 18:30:59 +0200

    feat(v1.0): make calorie calculator a class

---
+++ src/Helpers/CalorieCalculator.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace src\Helpers;
+
+use src\Entity\{Food, Products};
+
+class CalorieCalculator {
+    public function calorieCalc(Food|Products $food): int {
+        return $food->getProtein() * 4 + $food->getCarbs() * 4 + $food->getFats() * 9 + $food->getFiber() * 4;
+    }
+}
