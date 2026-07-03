be0666bb3f050316a4c625a0153ed866e2be0c47
ToniCoding <antoniovallejo2612@gmail.com>
2026-07-04 01:55:49 +0200

    feat(v1.1): adapt home page to jwt endpoints

--- src/Controller/HomePageController.php
+++ src/Controller/HomePageController.php
@@ -18,31 +18,7 @@ class HomePageController extends AbstractController {
 
     #[Route(['/', '/home'], name: 'home', methods: 'GET')]
     public function home(): Response {
-        $user = $this->getUser();
-
-        $userProgress = $this->userMacrosRetrieve->calculateUserProgress($user);
-        $userGoals = $this->userMacrosRetrieve->getMacroGoalsArr($user);
-        $macrosConsumed = $this->userMacrosRetrieve->getConsumedMacrosArr($user);
-        $weeklyGoal = $this->userMacrosRetrieve->getUserWeeklyCalorieGoal($user);
-        $weeklyConsumption = $this->userMacrosRetrieve->getCaloriesConsumedForCurrentWeek($user);
-        
-        $weeklyGoalDanger = $this->userMacrosRetrieve->calculateWeeklyRisk($weeklyGoal, $weeklyConsumption, $macrosConsumed['calories']);
-        $weeklyRisk = str_replace('_', ' ', ucfirst($weeklyGoalDanger['risk']));
-        $riskColor = $weeklyGoalDanger['risk_color'];
-
-        return $this->render('HomePageTemplate.twig.html', [
-            'message' => 'Welcome to SMC',
-            'caloricProgress' => $userGoals['caloriesGoal'] - $macrosConsumed['calories'],
-            'caloriesConsumed' => $macrosConsumed['calories'],
-            'calorieGoal' => $userGoals['caloriesGoal'],
-            'weeklyGoal' => $weeklyGoal,
-            'weeklyConsumption' => $weeklyConsumption,
-            'weeklyRisk' => $weeklyRisk,
-            'riskColor' => $riskColor,
-            ...$userProgress,
-            ...$userGoals,
-            ...$macrosConsumed
-        ]);
+        return $this->render('HomePageTemplate.twig.html');
     }
     
     /**
